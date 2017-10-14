a7468e70dc078b6f4a976950392533d07965fba8
ouzm_global <ouzm_global@atguigu.com>
2017-10-14 15:23:20 +0800

    common commit 1

--- src/main/java/com/atguigu/demo/User.java
+++ src/main/java/com/atguigu/demo/User.java
@@ -1,7 +1,11 @@
 package com.atguigu.demo;
 
 public class User {
+	static{
+		System.out.println("修改第一次：添加静态代码块！！！");
+	}
 	public static void main(String[] args) {
-		System.out.println("welcome to shenzheng");
+		System.out.println("welcome to shenzhen");
+		System.out.println("欢迎来深圳");
 	}
 }
